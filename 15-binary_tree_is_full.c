9ec1d6f52500093fb4b956966b87b6a4601a8464
Kenechukwu Akubue <kengoon19@gmail.com>
2024-03-27 23:38:33 +0100

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int is_full_recursive(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if ((tree->left && tree->!right) ||
+		if ((tree->left && !tree->right) ||
 		    (!tree->left && tree->right) ||
 		    is_full_recursive(tree->left) == 0 ||
 		    is_full_recursive(tree->right) == 0)
